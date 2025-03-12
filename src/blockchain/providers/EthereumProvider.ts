75b27ac5894b780ad5e17c059b5b8d39523d42e7
Lapo4kaKek <Sheydos2003@yandex.ru>
2025-03-12 19:44:25 +0300

    feat: implemented sendTransaction

--- src/blockchain/providers/EthereumProvider.ts
+++ src/blockchain/providers/EthereumProvider.ts
@@ -1,6 +1,15 @@
+import { TransactionResponse } from "ethers";
 import { BlockchainAccount } from "../types/account";
 import { AbstractProvider } from "./AbstractProvider";
 import { ethers } from "ethers";
+import dotenv from "dotenv";
+
+import logger from "../../utils/logger";
+
+dotenv.config();
+
+const provider = new ethers.JsonRpcProvider(process.env.ETH_RPC_URL);
+const wallet = new ethers.Wallet(process.env.PRIVATE_KEY!, provider);
 
 export class EthereumProvider extends AbstractProvider {
   private provider: ethers.JsonRpcProvider;
@@ -10,16 +19,16 @@ export class EthereumProvider extends AbstractProvider {
     super(rpcUrl);
     this.provider = new ethers.JsonRpcProvider(rpcUrl);
     this.signer = new ethers.Wallet(privateKey, this.provider);
-}
+  }
 
   connect(): Promise<void> {
     throw new Error("Method not implemented.");
   }
   async getBalance(address: string, tokenAddress?: string): Promise<bigint> {
     if (!tokenAddress) {
-        // native ETH balance
-        let response = await this.provider.getBalance(address);
-        return BigInt(response.toString());
+      // native ETH balance
+      let response = await this.provider.getBalance(address);
+      return BigInt(response.toString());
     }
     const abi = ""; // later fix
 
@@ -31,8 +40,21 @@ export class EthereumProvider extends AbstractProvider {
   getAccountInfo(address: string): Promise<BlockchainAccount> {
     throw new Error("Method not implemented.");
   }
-  sendTransaction(txData: any): Promise<string> {
-    throw new Error("Method not implemented.");
+  async sendTransaction(
+    tx: ethers.TransactionRequest
+  ): Promise<TransactionResponse | null> {
+    try {
+      logger.info(`Sending transaction: ${JSON.stringify(tx, null, 2)}`);
+
+      const signedTx = await wallet.sendTransaction(tx);
+      await signedTx.wait(); // Wait for confirmation
+
+      logger.info(`Transaction sent: ${signedTx.hash}`);
+      return signedTx;
+    } catch (error) {
+      logger.error("Transaction failed", error);
+      return null;
+    }
   }
   estimateGas(txData: any): Promise<bigint> {
     //let estimateData = this.provider.estimateGas(tx);
