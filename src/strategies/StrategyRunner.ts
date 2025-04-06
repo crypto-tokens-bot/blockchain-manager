aa079b1c8d00fa7b98490fea9843fd5d3d606a80
Lapo4kaKek <Sheydos2003@yandex.ru>
2025-04-07 01:36:18 +0300

    feat: added raw version strategy runner

---
+++ src/strategies/StrategyRunner.ts
@@ -0,0 +1,42 @@
+import { Queue, Worker, Job } from "bullmq";
+import { redisConnection } from "../queue/redis";
+import { handleDepositEvent } from "../strategies/depositStrategy";
+
+interface ContractEventData {
+    event: string;
+    contract: string;
+    data: any;
+    blockNumber: number;
+  }
+
+const strategyWorker = new Worker<ContractEventData>(
+  "event-queue",
+  async (job: Job<ContractEventData>) => {
+    if (!job) return;
+    const data = job!.data;
+    const { event, contract, blockNumber } = job.data;
+    console.log(`StrategyRunner: Received event ${event} from contract ${contract} at block ${blockNumber}`);
+
+    switch (event) {
+      case "Deposited":
+        await handleDepositEvent({ contract, data, blockNumber });
+        break;
+      default:
+        console.warn(`StrategyRunner: No strategy defined for event ${event}`);
+    }
+  },
+  { connection: redisConnection }
+);
+
+
+strategyWorker.on("completed", (job) => {
+  console.log(`Job ${job.id} processed successfully.`);
+});
+
+strategyWorker.on("failed", (job: Job<ContractEventData> | undefined, err: Error) => {
+  console.error(`Job ${job?.id} failed:`, err);
+});;
+
+export function runStrategyRunner() {
+  console.log("StrategyRunner is running...");
+}
