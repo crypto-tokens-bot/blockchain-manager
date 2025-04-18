2e3806d9d48b9eaa5afca8cafb6903a007032d4b
Lapo4kaKek <Sheydos2003@yandex.ru>
2025-04-18 20:11:14 +0300

    feat: added dependency for uniswap

---
+++ src/blockchain/dex/uniswap.ts
@@ -0,0 +1,32 @@
+import { ethers } from "ethers";
+import { Token, CurrencyAmount, TradeType, Percent } from "@uniswap/sdk-core";
+import { Pool, Route, SwapRouter } from "@uniswap/v3-sdk";
+
+export async function getQuote(
+  provider: ethers.JsonRpcProvider,
+  tokenIn: Token, tokenOut: Token,
+  amountIn: CurrencyAmount<Token>,
+  slippageTolerance: Percent
+) {
+  // 1. Получаем данные пула (sqrtPrice, liquidity)
+  // 2. Строим Route и Trade
+  // 3. Вычисляем amountOutMin = trade.minimumAmountOut(slippageTolerance)
+  // return { route, trade, amountOutMin };
+}
+
+export async function executeSwap(
+  signer: ethers.Signer,
+  route: Route<Token, Token>,
+  amountOutMin: CurrencyAmount<Token>,
+  deadline: number,
+  value?: bigint
+) {
+//   const { calldata } = SwapRouter.swapCallParameters(route, {
+//     //slippageTolerance,
+//     deadline,
+//     recipient: await signer.getAddress(),
+//     amountOutMinimum: amountOutMin.quotient.toString(),
+//     sqrtPriceLimitX96: 0n,
+//   });
+//   return signer.sendTransaction({ to: SwapRouter.ADDRESS, data: calldata, value });
+}
